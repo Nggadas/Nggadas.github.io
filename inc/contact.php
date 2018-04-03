189ffe4ec469e4fedfc3be35e7062dd378f8b0ef
Emmanuel Nggadas <enggadas80@gmail.com>
2018-04-04 00:14:03 +0100

    fixing email error

--- inc/contact.php
+++ inc/contact.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit'])){
         $mail = new PHPMailer(true);                    // Passing `true` enables exceptions
         try {
             //Server settings
-            $mail->SMTPDebug = 2;                               // Enable verbose debug output
+            $mail->SMTPDebug = 1;                               // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
