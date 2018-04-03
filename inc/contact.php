04007233324fe3597a2ca8eeaf1ba6c2087d5627
Emmanuel Nggadas <enggadas80@gmail.com>
2018-04-03 23:56:26 +0100

    fixing email error

--- inc/contact.php
+++ inc/contact.php
@@ -19,6 +19,10 @@ if(isset($_POST['submit'])){
         $message = $_POST["message"];
         $body = "<strong>Name:</strong>" . $user_name . "<br> <strong>Email:</strong> " . $user_email . "<br><br>" . $message;
 
+        //ENV Variables
+        $env_email = getenv('EMAIL');
+        $env_password = getenv('PASSWORD');
+
         //Load Composer's autoloader
         require 'vendor/autoload.php';
 
@@ -29,8 +33,8 @@ if(isset($_POST['submit'])){
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
-            $mail->Username = getenv('EMAIL');            // SMTP username
-            $mail->Password = getenv('PASSWORD');         // SMTP password
+            $mail->Username = $env_email;            // SMTP username
+            $mail->Password = $env_password;         // SMTP password
             $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587;                                    // TCP port to connect to
 
